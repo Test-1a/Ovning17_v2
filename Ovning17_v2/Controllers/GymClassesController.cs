85f81e37cf26bdc90108d93496a4015b25258ae1
Mikael <mikael@sehlin.net>
2020-01-19 00:29:18 +0100

    v0.3_1-5

--- Ovning17_v2/Controllers/GymClassesController.cs
+++ Ovning17_v2/Controllers/GymClassesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,6 +12,7 @@ using Ovning17_v2.Models;
 
 namespace Ovning17_v2.Controllers
 {
+    [Authorize]
     public class GymClassesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -23,6 +25,7 @@ namespace Ovning17_v2.Controllers
         }
 
         // GET: GymClasses
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             //            return View(await _context.GymClasses.ToListAsync());
--- Ovning17_v2/Startup.cs
+++ Ovning17_v2/Startup.cs
@@ -66,7 +66,7 @@ namespace Ovning17_v2
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=GymClasses}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }

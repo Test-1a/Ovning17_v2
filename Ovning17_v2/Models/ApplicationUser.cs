080cc353f66910cab474aa0a53382a26a8f0c1a2
Mikael <mikael@sehlin.net>
2020-01-21 00:43:46 +0100

    v0.5_1

--- Ovning17_v2/Models/ApplicationUser.cs
+++ Ovning17_v2/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;    //[Required] etc
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,19 @@ namespace Ovning17_v2.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [StringLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public DateTime TimeOfRegistration { get; set; }
+
+
         public IEnumerable<ApplicationUserGymClass> AttendedClasses { get; set; }
     }
 }
